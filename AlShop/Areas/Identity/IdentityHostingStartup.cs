cf0bb276a2ad08189bcd84ff24b815ae27b57840
TayfunF <firtinatayfun08@gmail.com>
2022-05-23 14:29:16 +0300

    Identity Scaffolding Item Added Areas Folder

---
+++ AlShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AlShop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AlShop.Areas.Identity.IdentityHostingStartup))]
+namespace AlShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
